dca7bc1768109174793cf6fefbc047597c52d38f
Nguyen Quy Dat <quydathaiduong@gmail.com>
2025-01-30 00:45:48 +0800

    add textList to store all text, add command list to print textList

--- src/main/java/DanChat.java
+++ src/main/java/DanChat.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 
 public class DanChat {
     public static void printMessage(String message) {
-        String line = "=============================";
-        System.out.println(line + "\n" + "\t" + message + "\n" + line);
+        System.out.println("=============================");
+        System.out.println("\t" + message);
+        System.out.println("=============================");
+    }
+    public static void printList(String[] items) {
+        int itemIndex = 1; //Output index for list items, starting from 1.
+        for (String item: items) {
+            if (item == null) {
+                break;
+            }
+            System.out.println("\t" + itemIndex + ". " + item);
+            itemIndex++;
+        }
     }
     public static void main(String[] args) {
         String line = "=============================";
@@ -17,14 +28,24 @@ public class DanChat {
         System.out.println(" > ^ <  " + " " + "What can I do for you?");
         System.out.println(line);
         String text;
+        String[] textList = new String[100];
+        int textCount = 0;
         Scanner in = new Scanner(System.in);
         text = in.nextLine();
         while (!text.equals("bye")) {
-            printMessage("added: " + text);
+            if (text.equals("list")) {
+                System.out.println(line);
+                printList(textList);
+                System.out.println(line);
+            }
+            else {
+                textList[textCount] = text;
+                textCount++;
+                printMessage("added: " + text);
+            }
             Scanner nextIn = new Scanner(System.in);
             text = nextIn.nextLine();
         }
-
         printMessage("Bye. Hope to see you again soon!");
     }
 }
