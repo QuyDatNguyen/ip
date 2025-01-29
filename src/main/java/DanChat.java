87e422b64e4df5baa28c9f488312a16110e241eb
Nguyen Quy Dat <quydathaiduong@gmail.com>
2025-01-30 00:21:02 +0800

    add output to print user's command line with exit command "bye"

--- src/main/java/DanChat.java
+++ src/main/java/DanChat.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
+
 public class DanChat {
+    public static void printMessage(String message) {
+        String line = "=============================";
+        System.out.println(line + "\n" + "\t" + message + "\n" + line);
+    }
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -6,8 +12,26 @@ public class DanChat {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm DanChat\n"+
-                "What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        String line = "=============================";
+//        System.out.println(line);
+//        System.out.println("Hello! I'm DanChat\n"+
+//                "What can I do for you?");
+//        System.out.println(line);
+        System.out.println(line);
+        System.out.println(" /\\_/\\  ");
+        System.out.println("( o.o ) " + " " + "Hello! I'm DanChat");
+        System.out.println(" > ^ <  " + " " + "What can I do for you?");
+        System.out.println(line);
+        String command;
+        Scanner in = new Scanner(System.in);
+        command = in.nextLine();
+        while (!command.equals("bye")) {
+//            System.out.println(line + "\n" + "\t" + command + "\n" + line);
+            printMessage(command);
+            Scanner nextIn = new Scanner(System.in);
+            command = nextIn.nextLine();
+        }
+//        System.out.println(line + "\n" + "\t" + "Bye. Hope to see you again soon!" + "\n" + line);
+        printMessage("Bye. Hope to see you again soon!");
     }
 }
