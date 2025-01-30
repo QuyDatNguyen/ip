1339cb2f1959fbd141b4f5ae7a527ce236ea68bd
Nguyen Quy Dat <quydathaiduong@gmail.com>
2025-01-30 22:19:23 +0800

    add class Task with done status, add command to mark and unmark tasks

--- src/main/java/DanChat.java
+++ src/main/java/DanChat.java
@@ -6,46 +6,82 @@ public class DanChat {
         System.out.println("\t" + message);
         System.out.println("=============================");
     }
-    public static void printList(String[] items) {
-        int itemIndex = 1; //Output index for list items, starting from 1.
-        for (String item: items) {
-            if (item == null) {
+    public static void printTaskList(Task[] tasks) {
+        int taskNumber = 1; //Output index for list items, starting from 1.
+        for (Task task: tasks) {
+            if (task == null) {
                 break;
             }
-            System.out.println("\t" + itemIndex + ". " + item);
-            itemIndex++;
+            String taskName = task.getDescription();
+            String taskStatusIcon = task.getStatusIcon();
+            System.out.println("\t" + taskNumber + ". " + "[" + taskStatusIcon + "] " + taskName);
+            taskNumber++;
+        }
+    }
+    public static boolean isValidInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
     public static void main(String[] args) {
         String line = "=============================";
-//        System.out.println(line);
-//        System.out.println("Hello! I'm DanChat\n"+
-//                "What can I do for you?");
-//        System.out.println(line);
         System.out.println(line);
         System.out.println(" /\\_/\\  ");
         System.out.println("( o.o ) " + " " + "Hello! I'm DanChat");
         System.out.println(" > ^ <  " + " " + "What can I do for you?");
         System.out.println(line);
+
         String text;
-        String[] textList = new String[100];
-        int textCount = 0;
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
+
         Scanner in = new Scanner(System.in);
         text = in.nextLine();
-        while (!text.equals("bye")) {
-            if (text.equals("list")) {
+
+        while (true) {
+            String[] words = text.split(" ");
+            if (text.trim().equals("bye")) {
+                printMessage("Bye. Hope to see you again soon!");
+                break;
+            }
+            else if (text.trim().equals("list")) {
                 System.out.println(line);
-                printList(textList);
+                printTaskList(tasks);
                 System.out.println(line);
+            } else if (words[0].equals("unmark") && words.length == 2 && isValidInteger(words[1])) {
+//                String[] words = text.split(" ");
+                int taskNumber = Integer.parseInt(words[1]);
+                if (taskNumber > taskCount) {
+                    printMessage("Sorry, there is no task with index " + taskNumber);
+                } else {
+                    Task changeTask = tasks[taskNumber - 1];
+                    changeTask.setDone(false);
+                    printMessage("Ok, I have marked this task as not done yet\n \t"
+                            + "[" + changeTask.getStatusIcon() + "] " + changeTask.getDescription());
+                }
+            } else if (words[0].equals("mark") && words.length == 2 && isValidInteger(words[1])) {
+//                String[] words = text.split(" ");
+                int taskNumber = Integer.parseInt(words[1]);
+                if (taskNumber > taskCount) {
+                    printMessage("Sorry, there is no task with index " + taskNumber);
+                } else {
+                    Task changeTask = tasks[taskNumber - 1];
+                    changeTask.setDone(true);
+                    printMessage("Nice! I have marked this task as done\n \t"
+                            + "[" + changeTask.getStatusIcon() + "] " + changeTask.getDescription());
+                }
             }
             else {
-                textList[textCount] = text;
-                textCount++;
+                Task t = new Task(text);
+                tasks[taskCount] = t;
+                taskCount++;
                 printMessage("added: " + text);
             }
             Scanner nextIn = new Scanner(System.in);
             text = nextIn.nextLine();
         }
-        printMessage("Bye. Hope to see you again soon!");
     }
 }
