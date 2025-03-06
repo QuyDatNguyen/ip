6c3b52521ce58b664a95eae2c940eedbfef2de86
QuyDatNguyen <142130714+QuyDatNguyen@users.noreply.github.com>
2025-03-06 13:31:56 +0800

    Update date with LocalDate format

--- src/main/java/danchat/task/Deadline.java
+++ src/main/java/danchat/task/Deadline.java
@@ -1,15 +1,19 @@
 package danchat.task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
-    protected String by;
+    protected LocalDate byDate;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.byDate = LocalDate.parse(by);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " by " + this.by;
+        return "[D]" + super.toString() + " by "
+                + this.byDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 }
